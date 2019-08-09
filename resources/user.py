2f4d62c5c29b7a8ad0ed4352acc23f982349b85b
unknown <lucasmalucha@gmail.com>
2019-08-09 20:08:31 +0100

    login

--- resources/user.py
+++ resources/user.py
@@ -1,76 +1,104 @@
-from flask import session, Response, render_template, redirect, flash
-from werkzeug.security import generate_password_hash, check_password_hash
+import traceback
+from flask import request, render_template, make_response
 from flask_restful import Resource
 from models.user import UserModel
-from libs.forms import RegisterForm, LoginForm
-from flask_login import LoginManager, login_user, logout_user, AnonymousUserMixin
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity, \
+    jwt_required, get_raw_jwt
+from marshmallow import ValidationError
+from blacklist import BLACKLIST
+from schemas.user import UserSchema
+from libs.mailgun import MailGunException
+from models.confirmation import ConfirmationModel
+from libs.strings import gettext
 
-login_manager = LoginManager()
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return UserModel.find_by_id(int(user_id))
-
-
-class Anonymous(AnonymousUserMixin):
-    def __init__(self):
-        self.email = 'Guest'
-
-
-login_manager.anonymous_user = Anonymous
+user_schema = UserSchema()
 
 
 class UserRegister(Resource):
 
-    def get(self):
-        form = RegisterForm()
-        return Response(render_template('user/register.html', form=form))  ## passing signup form to signup template
+    @classmethod
+    def post(cls):
+        user = user_schema.load(request.get_json())
 
-    def post(self):
-        form = RegisterForm()
-        if form.validate_on_submit():
-            if UserModel.find_by_email(form.email.data) or UserModel.find_by_username(form.username.data):
-                flash(f'User already exist', 'alert alert-danger')
-                return Response(render_template('user/register.html', form=form))
+        if UserModel.find_by_username(user.username):
+            return {"message": gettext("user_username_exists")}, 400
 
-            hashed_password = generate_password_hash(form.password.data,
-                                                     method='sha256')  ## password get hashed for security purposes
-            new_user = UserModel(email=form.email.data, username=form.username.data, password=hashed_password)
-            new_user.save_to_db()
-            login_user(new_user)
-            return redirect("/")
+        if UserModel.find_by_email(user.email):
+            return {"message": gettext("user_email_exists")}, 400
 
-        return Response(render_template('user/register.html', form=form))  ## passing signup form to signup template
+        try:
+            user.save_to_db()
+            confirmation = ConfirmationModel(user.id)
+            confirmation.save_to_db()
+            user.send_confirmation_email()
+            return {"message": gettext("user_registered")}, 201
+        except MailGunException as e:
+            user.delete_from_db()
+            return {"message": str(e)}, 500
+        except:  # user failed to save to db
+            traceback.print_exc()
+            user.delete_from_db()
+            return {"message": gettext("user_error_creating")}, 500
+
+
+class User(Resource):
+
+    @classmethod
+    def get(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': gettext("user_not_found")}, 404
+        return user_schema.dump(user), 200
+
+    @classmethod
+    def delete(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': gettext("user_not_found")}, 404
+        user.delete_from_db()
+        return {'message': gettext("user_deleted")}, 200
 
 
 class UserLogin(Resource):
 
-    def get(self):
-        form = LoginForm()
+    @classmethod
+    def post(cls):
+        user_json = request.get_json()
+        user_data = user_schema.load(user_json, partial=('email',))  # no need for email in login if not present
 
-        # alert alert-success
-        return Response(render_template('user/login.html', form=form))  ## passing login form to login template
+        user = UserModel.find_by_username(user_data.username)
 
-    def post(self):
-        form = LoginForm()
+        if user and safe_str_cmp(user.password, user_data.password):
+            # Check if user is activated
+            confirmation = user.most_recent_confirmation
+            if confirmation and confirmation.confirmed:
+                access_token = create_access_token(identity=user.id, fresh=True)
+                refresh_token = create_refresh_token(user.id)
+                return {
+                           'access_token': access_token,
+                           'refresh_token': refresh_token
+                       }, 200
+            return {"message": gettext("user_not_confirmed").format(user.username)}, 400
 
-        if form.validate_on_submit():  ## if form was submitted....
-            user = UserModel.find_by_email(email=form.email.data)
-            if user:
-                if check_password_hash(user.password, form.password.data):
-                    session['current_user'] = user.email
-                    flash(f'You have successfully logged in as {user.email}', 'alert alert-success')
-                    login_user(user)
-                    return redirect("/")
-            else:
-                flash(u'Invalid Email or Password provided', 'alert alert-danger')
-
-        return Response(render_template('user/login.html', form=form))
+        return {'message': gettext("user_invalid_credentials")}, 401
 
 
 class UserLogout(Resource):
+    @classmethod
+    @jwt_required
+    def post(cls):
+        """Blacklist used token"""
+        jti = get_raw_jwt()['jti']  # jti is JWT ID
+        BLACKLIST.add(jti)
+        return {'message': gettext("user_logged_out")}, 200
 
-    def get(self):
-        logout_user()
-        return redirect("login")
+
+class TokenRefresh(Resource):
+    @classmethod
+    @jwt_refresh_token_required
+    def post(cls):
+        """Renewing token"""
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
