58e0d09c724edfe40809c28ab483df287e7ebb24
unknown <lucasmalucha@gmail.com>
2019-08-13 21:24:16 +0100

    get

--- resources/confirmation.py
+++ resources/confirmation.py
@@ -1,41 +1,14 @@
-from flask import make_response, render_template, request, url_for, Response
+from flask import render_template, request, url_for, Response
 from flask_restful import Resource
 from libs.strings import gettext
 from models.confirmation import ConfirmationModel
-from schemas.confirmation import ConfirmationSchema
-
-confirmation_schema = ConfirmationSchema()
-
-
-class Confirmationasd(Resource):
-    @classmethod
-    def get(cls, confirmation_id):
-        """Return confirmation HTML page."""
-        confirmation = ConfirmationModel.find_by_id(confirmation_id)
-
-        if not confirmation:
-            return {"message": gettext("confirmation_not_found"), 'status': 404}
-
-        if confirmation.expired:
-            return {"message": gettext("confirmation_link_expired"), 'status': 404}
-
-        if confirmation.confirmed:
-            return {"message": gettext("confirmation_already_confirmed"), 'status': 404}
-
-        confirmation.confirmed = True
-        confirmation.save_to_db()
-        try:
-            confirmation.save_to_db()
-            return {'email': confirmation.user.email, 'status': 200}
-        except:
-            confirmation.confirmed = False
-            return {"message": gettext("confirmation_confirm_error"), 'status': 500}
 
 
 class Confirmation(Resource):
     @classmethod
     def get(cls, confirmation_id):
-        return Response(render_template('user/confirmation.html', confirmation_id=confirmation_id))
+        link = request.url_root[:-1] + url_for("confirmation", confirmation_id=confirmation_id)
+        return Response(render_template('user/confirmation.html', confirmation_id=confirmation_id, link=link))
 
 
     @classmethod
@@ -61,5 +34,3 @@ class Confirmation(Resource):
             confirmation.confirmed = False
             return {"message": gettext("confirmation_confirm_error"), 'status': 500}
 
-        # link = request.url_root[:-1] + url_for("confirmation", confirmation_id=confirmation_id)
-        # return make_response(render_template("user/confirmation_email.html", link=link, confirmation_id=confirmation_id, conf=conf))
