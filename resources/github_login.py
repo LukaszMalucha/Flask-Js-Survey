c6d32ae4a48158ff1c2c9f2b18f89a2e7729e84b
unknown <lucasmalucha@gmail.com>
2019-08-23 18:36:51 +0100

    oauth

--- resources/github_login.py
+++ resources/github_login.py
@@ -28,15 +28,15 @@ class GithubAuthorize(Resource):
             return error_response
 
         g.access_token = resp['access_token']  # put access token inside flask_global
-        # github_user = github.get('user')
-        # github_email = github_user.data['login']
-        #
-        # user = UserModel.find_by_username(github_email)
-        #
-        # if not user:
-        #     user = UserModel(username=github_email, email=github_email,
-        #                      password=None)  # if login with github, there is no password given
-        #     user.save_to_db()
-        #
-        # login_user(github_email)
-        return {"access_token": "1"}
+        github_user = github.get('user')
+        github_email = github_user.data['login']
+
+        user = UserModel.find_by_username(github_email)
+
+        if not user:
+            user = UserModel(username=github_email, email=github_email,
+                             password=None)  # if login with github, there is no password given
+            user.save_to_db()
+
+        login_user(user)
+        return redirect('/')
