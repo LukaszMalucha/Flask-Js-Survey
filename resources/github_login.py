82546650790df66805e0d071f9120830ef7d2f40
unknown <lucasmalucha@gmail.com>
2019-08-27 19:25:27 +0100

    data

--- resources/github_login.py
+++ resources/github_login.py
@@ -1,4 +1,4 @@
-from flask import g, request, url_for, redirect, session, render_template
+from flask import g, request, url_for, redirect, session
 from flask_login import login_user
 from flask_restful import Resource
 
